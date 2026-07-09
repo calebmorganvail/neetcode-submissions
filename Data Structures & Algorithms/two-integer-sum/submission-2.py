d700ba38f757c063433acef96189a03db6195739
Caleb Vail <143128562+calebmorganvail@users.noreply.github.com>
2026-07-09 13:23:15 -0600

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute force O(n^2)
+        # for r, num in enumerate(nums):
+        #     remainder = target - num
+        #     if remainder in nums and nums.index(remainder) != r:
+        #         i, j = nums.index(remainder), r
+        # return [i, j]
+        
+
+        # Also O(n^2)
+        # seen = set()
+
+        # for r, num in enumerate(nums):
+        #     remainder = target - num
+        #     if remainder in seen:
+        #         i = nums.index(remainder) # This goes over the list again. 
+        #         j = r
+        #     seen.add(num)
+        # return [i, j]
+
+        seen = {}
+        for r, num in enumerate(nums):
+            remainder = target - num
+            if remainder in seen:
+                return [seen[remainder], r]
+            seen[num] = r
+        
