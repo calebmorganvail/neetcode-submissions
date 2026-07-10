ff6231e2a23a37a6039681a84f297417d6a71024
Caleb Vail <143128562+calebmorganvail@users.noreply.github.com>
2026-07-10 14:43:35 -0600

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1 = [char.lower() for char in s if char.isalnum()]
+        s2 = [char.lower() for char in s if char.isalnum()]
+        print(s1)
+        for char in s1:
+            if s1.pop() != s2.pop(0):
+                return False
+        
+        return True
+
+        
